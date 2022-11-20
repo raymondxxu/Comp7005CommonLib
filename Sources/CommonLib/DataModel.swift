86e165aaf0b63834aa14848451d4edede718b107
Raymond <raymondxxu>
2022-11-20 13:47:19 -0800

    Adding help functions

--- Sources/CommonLib/DataModel.swift
+++ Sources/CommonLib/DataModel.swift
@@ -5,13 +5,20 @@ public enum ModelType: String, Codable{
     case ASK
 }
 
+protocol JsonStringConvertible {
+    
+    func convert(from json: String) -> JsonStringConvertible?
+    func convert() -> String? 
+
+}
+
 public struct DataModel {
     
     public private (set) var type: ModelType
-    public private (set) var data: String
+    public private (set) var data: String?
     public private (set) var id: UInt
 
-    public init(seq:UInt, type: ModelType, data: String) {
+    public init(seq:UInt, type: ModelType, data: String?) {
         self.id = seq
         self.type = type 
         self.data = data
@@ -32,7 +39,23 @@ extension DataModel: Codable {
 extension DataModel: CustomStringConvertible {
     
     public var description: String {
-        "SequenceNumber: \(id), \(type), Content: \(data)"
+        "SequenceNumber: \(id), \(type), Content: \(data ?? "")"
+    }
+
+}
+
+extension DataModel: JsonStringConvertible {
+    
+    func convert(from json: String) -> JsonStringConvertible? {
+        let decoder = JSONDecoder()
+        return try? decoder.decode(DataModel.self, from: Data(json.utf8))
+    }
+
+    func convert() -> String? {
+        let encoder = JSONEncoder()
+        let data = try? encoder.encode(self)
+        guard let data = data else { return nil }
+        return String(data: data, encoding: .utf8)
     }
 
 }
